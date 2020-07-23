a8c159efec179915ef339f6876d5477acf8ddda6
Your Name <you@example.com>
2020-07-23 21:34:06 +0600

    Adding homework

---
+++ Homework (number classification)/Number Classification.c
@@ -0,0 +1,60 @@
+/*Write a C code that takes a number as input from user and determines whether
+it's a Positive Real/Negative Real/Natural/Rational/Irrational Number
+and gives The feedback to user.*/
+
+
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float num;
+    printf("Enter a number: ");
+    scanf("%f", &num);
+
+    if (num > 0.0)
+       {
+        if((num - (int)num)==0)
+           printf("You entered a Natural number.");
+        else if ((num-sqrt(pow(num,2)))==0)
+           printf("You entered a Positive Rational number");
+        else
+           printf ("You entered an Irrational number");
+       }
+    else if (num < 0.0)
+            {
+             if((num-(int)num)==0)
+               printf("You entered a Negative Integer");
+             else if((num+sqrt(pow(num,2)))==0)
+               printf("you entered a Negative Rational number");
+             else
+               printf("You entered a Negative Irrational number");
+            }
+    else
+       printf("You entered zero");
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
