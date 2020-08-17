2fda9b660b02a9631b270482c53fe9852937acca
Your Name <you@example.com>
2020-08-18 01:38:56 +0600

    Uploading Week 06-Lab 09 problems on loop

--- Week 06-Lab 09/Problems (loop)/3.Number pattern.c
+++ Week 06-Lab 09/Problems (loop)/3.Number pattern.c
@@ -1,15 +1,27 @@
 //Printing a certain number pattern
 
 #include<stdio.h>
+void numberPatternPrint(int N);    //Declaring function for printing pattern
+
 int main()
 {
-    int i, j, n;                   //Declaring integer variables
+    int n;                         //Declaring integer variables
 
     printf("Enter a number:");
     scanf("%d", &n);               //Taking inputs
     printf("\n\n");
 
-    for(i=1;i<=n;i++)
+    numberPatternPrint(n);         //using the void function to print the pattern
+
+    return 0;
+}
+
+//The void numberPatternPrint(int N) function
+void numberPatternPrint(int N)
+{
+    int i, j;
+
+    for(i=1;i<=N;i++)
     {
        if(i%2==1)                  //Using if else statment and modulas to differentiate between odd and even
          for(j=1;j<=(i+1)/2;j++)
@@ -25,8 +37,6 @@ int main()
          }
        printf("\n");
     }
-
-    return 0;
 }
 
 
